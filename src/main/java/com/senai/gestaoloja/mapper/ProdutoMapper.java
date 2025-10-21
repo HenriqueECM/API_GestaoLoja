1912850ba66daba62cfda62c5cd26e816d03ec35
henrique_menel <henriqueworks68@gmail.com>
2025-10-21 19:12:58 -0300

    feat: adicionado a verificação do update no mapper produto para validação antes de atualizar um produto

--- src/main/java/com/senai/gestaoloja/mapper/ProdutoMapper.java
+++ src/main/java/com/senai/gestaoloja/mapper/ProdutoMapper.java
@@ -8,7 +8,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProdutoMapper {
     public Produto paraEntidade(ProdutoRequest request){
+        if (request == null) {
+            throw new IllegalArgumentException("Requisição de produto não pode ser nula.");
+        }
         return new Produto(request.nome(), request.descricao(), request.preco(), request.quantidade());
+    }
+
+    public Produto verificarUpdate(ProdutoRequest request, Produto produto) {
+        if (request.nome() != null && !request.nome().equals(produto.getNome())) {
+            produto.setNome(request.nome());
+        }
+
+        if (request.descricao() != null && !request.descricao().equals(produto.getDescricao())) {
+            produto.setDescricao(request.descricao());
+        }
+
+        if (request.preco() != null && !request.preco().equals(produto.getPreco())) {
+            produto.setPreco(request.preco());
+        }
+
+        if (request.quantidade() != null && !request.quantidade().equals(produto.getQuantidade())) {
+            produto.setQuantidade(request.quantidade());
+        }
+
+        return produto;
     }
 
     public ProdutoResponse paraResposta(Produto produto){
