9bbe3895338d32a3b5b68a760b87592ce6f05898
henrique_menel <henriqueworks68@gmail.com>
2025-10-21 19:11:17 -0300

    feat: adicionado validações no request do produto

--- src/main/java/com/senai/gestaoloja/dto/request/ProdutoRequest.java
+++ src/main/java/com/senai/gestaoloja/dto/request/ProdutoRequest.java
@@ -1,9 +1,24 @@
 package com.senai.gestaoloja.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record ProdutoRequest(
+        // colocar validações aqui
+        @NotNull(message = "O nome deve ser informado.")
+        @NotBlank(message = "O nome do produto é obrigatório.")
         String nome,
+
+        @NotBlank
         String descricao,
+        @NotNull(message = "O preço deve ser informado.")
+        @Positive(message = "O preço deve ser maior que zero.")
         Double preco,
+
+        @NotNull(message = "A quantidade deve ser informado.")
+        @Positive(message = "A quantidade deve ser maior que zero.")
         Integer quantidade
 ) {
 }
